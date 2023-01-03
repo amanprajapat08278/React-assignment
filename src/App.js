1a3789224b87917bf1a1505acd1abd9a37ecaf3d
amanprajapat08278 <amanprajapat08278@gmail.com>
2023-01-03 21:15:28 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-
+import React from 'react';
 import './App.css';
 
-function App(props) {
-  const run = () => {
-    console.log("Button has clicked")
+function App() {
+  
+  
+  const run = () =>{
+    let value = document.getElementById("text").value
+    let Upper = value.toUpperCase()
+    document.getElementById("para").innerHTML = Upper
   }
   return (
     <>
       <div className="box">
-        <img id='logo' src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c51f.png' alt='Loading' />
-        <input id='search' />
-        <div className="btnBox">
-          <button id='btn1' className='btn' onClick={run}>Google Search</button>
-          <button id='btn2' className='btn' onClick={run}>I'm Feeling Lucky</button>
-        </div>
-        <p id='para'>Google offer in : <a id='lang' href='/'>{props.lang}</a></p>
+        <h3 id="heading">Write Your Bio</h3>
+        <textarea type="text" id="text" />
+        <button id="btn" onClick={() => run()}>Upper Case</button>
+        <span id="para"></span>
       </div>
     </>
   );
