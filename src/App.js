3833200b2b500ad110d8fc9959d9bd79c01df70c
amanprajapat08278 <amanprajapat08278@gmail.com>
2023-01-04 20:59:26 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  
+
   let [fname, setfname] = useState("")
   let [lname, setlname] = useState("")
   let [mobile, setmobile] = useState("")
   let [country, setcountry] = useState("")
-  
-    const run = (e)=>{
-     e.preventDefault();
-     const newContent = {firstName:fname, lastName:lname, mobile:mobile, country:country}
-     let check = document.getElementById("check").checked;
-     let gender = document.getElementById("gneder").value
-     newContent.gender = gender
-     newContent.marketingEmails = check
-     console.log(newContent)
+  const [error, setError] = useState("")
+  const [error1, setError1] = useState("")
+  const [error2, setError2] = useState("")
+  const [error3, setError3] = useState("")
+
+  const run = (e) => {
+    e.preventDefault();
+    let newContent = {}
+    if (!fname) { return setError("first name is mendatory & valid") }
+    else {
+      setError("")
+      newContent.firstName = fname
+    }
+    if (!lname) {return setError1("last name is mendatory & valid") }
+    else {
+      setError("")
+      newContent.lastName = lname
+    }
+    if (!mobile) {return setError2("mobile nmber is mendatory & valid") }
+    else {
+      setError("")
+      newContent.Mobile = mobile
+    }
+    if (!country) {return setError3("country is mendatory & valid") }
+    else {
+      setError("")
+      newContent.Country = country
+    }
+    let check = document.getElementById("check").checked;
+    let gender = document.getElementById("gneder").value
+    newContent.gender = gender
+    newContent.marketingEmails = check
+
+    console.log(newContent)
+
+    setcountry("")
+    setfname("")
+    setlname("")
 
 
-     
+
+
   };
   function PreviewImage() {
     var oFReader = new FileReader();
@@ -32,36 +62,40 @@ function App() {
     <>
       <form className="box" onSubmit={run}>
 
+
+
+        <div className="smallBox" id='imgBox'>
+          <img id="uploadPreview" alt='No Profile' />
+          <span className='atr' id='profile'>Profile</span>
+          <input className="inputs" id="uploadImage" type="file" accept='Image' onChange={PreviewImage} />
+          {/* {error}  */}
+        </div>
+
         <div className="smallBox" onSubmit={run}>
-            <span className='atr'>First Name</span>
-            <input className="inputs" id="fname" type="text" placeholder="Write your first name" name='fname' value={fname} onChange={(e)=>setfname(e.target.value)}/>
-            {/* {error} */}
+          <span className='atr'>First Name</span>
+          <input className="inputs" id="fname" type="text" placeholder="Write your first name" name='fname' value={fname} onChange={(e) => setfname(e.target.value)} />
+          {error}
         </div>
 
         <div className="smallBox">
-            <span className='atr'>Last Name</span>
-            <input className="inputs" id="lname" type="text" placeholder="Write your last name" name='fname' value={lname} onChange={(e)=>setlname(e.target.value)}/>
-            {/* {error} */}
+          <span className='atr'>Last Name</span>
+          <input className="inputs" id="lname" type="text" placeholder="Write your last name" name='fname' value={lname} onChange={(e) => setlname(e.target.value)} />
+          {error1}
         </div>
 
         <div className="smallBox">
-            <span className='atr'>Mobile</span>
-            <input className="inputs" id="mobile" type="number" placeholder="Write your mobile number" value={mobile} onChange={(e)=>setmobile(e.target.value)}/>
-            {/* {error} */}
+          <span className='atr'>Mobile</span>
+          <input className="inputs" id="mobile" type="number" placeholder="Write your mobile number" value={mobile} onChange={(e) => setmobile(e.target.value)} />
+          {error2}
         </div>
 
         <div className="smallBox">
-            <span className='atr'>Country</span>
-            <input className="inputs" id="country" type="text" placeholder="Write your country" value={country} onChange={(e)=>setcountry(e.target.value)}/>
-            {/* {error} */}
+          <span className='atr'>Country</span>
+          <input className="inputs" id="country" type="text" placeholder="Write your country" value={country} onChange={(e) => setcountry(e.target.value)} />
+          {error3}
         </div>
 
-        <div className="smallBox">
-        <img id="uploadPreview" alt='loading' />
-            <span className='atr'>Profile</span>
-            <input className="inputs" id="uploadImage" type="file" accept='Image' onChange={PreviewImage}/>
-            {/* {error} */}
-        </div>
+
 
         <div className="genBox">
           <span className='atr'>Gender : </span>
